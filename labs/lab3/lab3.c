9c50ff6b2e79554b59e912051a40b5e7e592975a
Nikhil Bagde <nbagde1@cs.binghamton.edu>
2016-06-09 22:32:11 -0400

    New version -Added lab3.c makefile, myStrStr.c

--- labs/lab3/lab3.c
+++ labs/lab3/lab3.c
@@ -1,22 +1,56 @@
 #include "myStrStr.c"
 #include<stdio.h>
 
+void printResultMessage(int result, char haystack[50], char niddle[50], char buffer[50]){
+        if(result)
+                printf("\tNiddle=[%s] matched with haystack=[%s]!, buffer=[%s]\n\n", niddle, haystack, buffer);
+        else
+                printf("\tSorry! Niddle=[%s] doens't matched with haystack=[%s], buffer=[%s]\n\n", niddle, haystack, buffer);
+}
 int main(){
 	//char haystack[50]={0}, needle[50]={0}, buffer[50]={0};
 	char buffer[50]={0};
 	//FILE * fptr;
-	int result = 0;
+	int result = 0, i = 0;
+
+	char *haystack[10] = {"apple","orange","blueberry","strawberry","banana","grapefruit"};
+	char *niddle[10] = {"app","ge","ueber","strawberry","na","terrible"};
+
+	for(i=0; i<6; i++){
+		result = myStrStr(haystack[i],niddle[i],buffer);
+		printResultMessage(result, haystack[i], niddle[i], buffer);
+		memset(buffer, 0, 50);
+	}
+/*
+	//1:
 	result = myStrStr("apple", "app", buffer);
-	if(result) printf("%s",buffer);
+	printResultMessage(result,buffer);
+	memset(buffer, 0, 50);
+
+	//2:
 	result = myStrStr("orange", "ge", buffer);
-	if(result) printf("%s",buffer);
+	printResultMessage(result,buffer);
+	memset(buffer, 0, 50);
+
+	//3:
 	result = myStrStr("blueberry", "ueber", buffer);
-	if(result) printf("%s",buffer);
+	printResultMessage(result,buffer);
+	memset(buffer, 0, 50);
+
+	//4:
 	result = myStrStr("strawberry", "strawberry", buffer);
-	if(result) printf("%s",buffer);
+	printResultMessage(result,buffer);
+	memset(buffer, 0, 50);
+
+	//5:
 	result = myStrStr("banana", "na", buffer);
-	if(result) printf("%s",buffer);
+	printResultMessage(result,buffer);
+	memset(buffer, 0, 50);
+
+	//6:
 	result = myStrStr("grapefruit", "terrible", buffer);
-	if(result) printf("%s",buffer);
+	printResultMessage(result,buffer);
+	memset(buffer, 0, 50);
+*/
 	return 0;
 }
--- labs/lab3/myStrStr.c
+++ labs/lab3/myStrStr.c
@@ -1,10 +1,43 @@
 #include<stdio.h>
+#include<string.h>
 
-#define DEBUG 1
+#define DEBUG 0
+
 int myStrStr(char haystack[50], char niddle[50], char buffer[50]){
 
-#ifdef DEBUG
-	printf("%s\n%s\n%s\n", haystack, niddle, buffer);
+#if DEBUG
+	printf("\t*** hay[%s]: Size=%d, nid[%s]:Size=%d, buf[%s]:Size=%d\n", haystack, strlen(haystack), niddle, strlen(niddle), buffer, strlen(buffer));
 #endif
-	return 0;
+	int i = 0, j = 0, k = 0, storeIndex = 0, flagContinuation = 0;
+	buffer[50] = 0;
+	for(i=0; i<strlen(niddle); i++){
+		char c = niddle[i];
+#if DEBUG
+		printf("\tniddle [%d] =  '%c' ",i,c);
+#endif
+		for(j = storeIndex; j<strlen(haystack); j++){
+			if(c == haystack[j]){
+#if DEBUG
+				printf("Matched!, haystack's [%d] = '%c'\n", j, haystack[j]);
+				printf("\tStored index=%d\n",j);
+#endif
+
+				buffer[k] = haystack[j];	//TODO:later try to combine  k++
+				k++;
+				storeIndex = ++j;
+				flagContinuation = 1;
+				break;
+			}else{
+				flagContinuation = 0;
+				continue;
+			}
+		}//haystack for
+#if DEBUG
+		printf("\n");					//added to format debugging when 2nd for loop desnt execute, first print of niddle was not formatter. adding \n in case 2nd for loop if never executes.
+#endif
+	}//niddle for
+#if DEBUG
+	printf("\n\tBuffer = %s\n\n", buffer);
+#endif
+	return (strlen(buffer)==strlen(niddle) ? 1:0);
 }
